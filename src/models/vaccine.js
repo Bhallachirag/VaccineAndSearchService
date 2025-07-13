a7106313485b5085baeaa348afc57b90bd9014c6
Chirag Bhalla <chiragbhalla73@gmail.com>
2025-07-13 14:02:44 +0530

    Added more routes to vaccine api

--- src/models/vaccine.js
+++ src/models/vaccine.js
@@ -14,9 +14,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Vaccine.init({
-    name: DataTypes.STRING,
-    allowNull: false,
-    unique: true
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true  
+    }
   }, {
     sequelize,
     modelName: 'Vaccine',
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,9 @@ const VaccineController = require('../../controllers/vaccine-controller');
 const router = express.Router();
 
 router.post('/vaccine', VaccineController.create);
+router.delete('/vaccine/:id', VaccineController.destroy);
+router.patch('/vaccine/:id', VaccineController.update);
+router.get('/vaccine/:id', VaccineController.get);
+
 
 module.exports = router;
